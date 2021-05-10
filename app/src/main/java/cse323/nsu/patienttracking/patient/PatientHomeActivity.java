c1d7d5d5e14ab9a1a54a83dc74ea0ab16962ddc1
Samiur Prapon <samiur.prapon@gmail.com>
2021-05-10 17:26:53 +0600

    patient logout feature added.

--- app/src/main/java/cse323/nsu/patienttracking/patient/PatientHomeActivity.java
+++ app/src/main/java/cse323/nsu/patienttracking/patient/PatientHomeActivity.java
@@ -1,16 +1,52 @@
 package cse323.nsu.patienttracking.patient;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.FirebaseAuth;
 
 import cse323.nsu.patienttracking.R;
+import cse323.nsu.patienttracking.authentication.AuthenticationActivity;
+import cse323.nsu.patienttracking.utils.CustomProgressBar;
 
 public class PatientHomeActivity extends AppCompatActivity {
+
+    MaterialButton mLogout;
+    private CustomProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
         setContentView(R.layout.activity_patient_home);
+
+        mLogout = findViewById(R.id.mb_logout);
+
+        progressBar = new CustomProgressBar(this);
+
+        mLogout.setOnClickListener(view -> {
+
+            progressBar.show("Logging out...");
+
+            new Handler(Looper.myLooper()).postDelayed(() -> {
+                FirebaseAuth.getInstance().signOut();
+
+                progressBar.hide();
+
+                pavilion();
+
+            }, 800);
+        });
+    }
+
+    private void pavilion() {
+        Intent intent = new Intent(PatientHomeActivity.this, AuthenticationActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
